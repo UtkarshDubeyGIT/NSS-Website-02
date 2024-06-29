7fdf5e17b0d69a7ca59adfec59bf48508ed2f0fc
Utkarsh Dubey <utkarsh.dubey.ug23@nsut.ac.in>
2024-06-30 00:34:45 +0530

    updated navbar and footer

--- src/script-home.js
+++ src/script-home.js
@@ -3,10 +3,14 @@ function toggleMenu() {
     const menu = document.getElementById('menu-list-items');
     const menuBarOpen = document.getElementById('menu-bar-open');
     const menuBarClose = document.getElementById('menu-bar-close');
-
+    const menubackground2 = document.getElementById('menu-background-2');
+    
+    const menubackground1 = document.getElementById('menu-background-1');
     menu.classList.toggle('hidden'); // Toggle the "hidden" class
 
     // Toggle the menu icon visibility
     menuBarOpen.classList.toggle('hidden');
     menuBarClose.classList.toggle('hidden');
+    menubackground2.classList.toggle('backdrop-blur-3xl');
+    menubackground1.classList.toggle('bg-white')
 }
